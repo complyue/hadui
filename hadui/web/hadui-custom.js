a435cd1faa19650d39e64cf6a9048838297d9d4c
Compl Yue <compl.yue@icloud.com>
2019-10-21 13:01:49 +0800

    rephrase

--- hadui/web/hadui-custom.js
+++ hadui/web/hadui-custom.js
@@ -8,7 +8,9 @@ export const HaduiDefaultStmt =
   // the statement shown initially
   `
 {- put and execute interactive Haskell statement here.
-you have the same artifacts in scope as \`stack ghci\`. -}
+you have the same artifacts in scope as \`stack ghci\`,
+with the \`RIO\` module implicitly imported in addition,
+so you get \`print\`, \`uiLog\` etc. -}
 print "Hello, web front!"
 `.trim();
 
