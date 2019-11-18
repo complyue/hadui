ff7fbefd2daf76eebc695a228f61059d3d5d308f
ComplYue <compl.yue@icloud.com>
2019-11-18 19:08:18 +0800

    don't rename the ghc compiler in nix space; simplify the package set override to @cdepillabout's suggestion

--- nixpkgs-overlays/ghc865ife-overlay.nix
+++ nixpkgs-overlays/ghc865ife-overlay.nix
@@ -1,5 +1,5 @@
 # This is the Nix overlay to arm an experimental branch of GHC
-# as the default compiler, together with the full Haskell package
+# as the default compiler, together with a full Haskell package
 # set (i.e. `haskellPackages`) with it.
 #
 # The compiler will be compiled from a source distribution, full
@@ -8,40 +8,24 @@
 self: super:
 let
 
-  # mock the env at (<nixpkgs> + /pkgs/top-level/haskell-packages)
-  callPackage = super.newScope {
-    haskellLib = super.haskell.lib;
-    overrides = super.pkgs.haskell.packageOverrides;
-  };
-
   # the compiler
   compiler865ife = super.haskell.compiler.ghc865.overrideAttrs (oldAttrs: {
-
-# use the official tarbal for reprod in
-#   https://github.com/NixOS/nixpkgs/issues/73443
-src = super.fetchurl {
-  url = "https://downloads.haskell.org/~ghc/8.6.5/ghc-8.6.5-src.tar.xz";
-  sha256 = "0qg3zsmbk4rkwkc3jpas3zs74qaxmw4sp4v1mhsbj0a0dzls2jjd";
-};
-
+    # renaming the compiler will fail later compilations with it,
+    # as described at https://github.com/NixOS/nixpkgs/issues/73443
+    # so do NOT do it!
     # name = "${oldAttrs.name}-ife";
 
-    # src = super.fetchurl {
-    #   name = "ghc-8.6.5-ife-src.tar.xz";
-    #   url =
-    #     "https://gitlab.haskell.org/complyue/ghc-ife-sdist/raw/master/ghc-8.6.5-src.tar.xz";
-    #   sha256 = "0wf5v1ry3rlwhbsxlvka3qscdb4jz4jn7w3jckvwysh0fm1bavs5";
-    # };
+    src = super.fetchurl {
+      url =
+        "https://gitlab.haskell.org/complyue/ghc-ife-sdist/raw/master/ghc-8.6.5-src.tar.xz";
+      sha256 = "0wf5v1ry3rlwhbsxlvka3qscdb4jz4jn7w3jckvwysh0fm1bavs5";
+    };
   });
 
   # the package set
-  haskellPackages865ife =
-    callPackage (<nixpkgs> + /pkgs/development/haskell-modules) {
-      buildHaskellPackages = super.buildPackages.haskell.packages.ghc865ife;
-      ghc = compiler865ife;
-      compilerConfig = callPackage (<nixpkgs>
-        + /pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix) { };
-    };
+  haskellPackages865ife = super.haskell.packages.ghc865.override {
+    ghc = compiler865ife;
+  };
 
 in {
 
